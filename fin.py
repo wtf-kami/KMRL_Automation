555354f1edbf6e0b236fdaff423c6ae21ba4dc78
wtfkami <116031067+wtf-kami@users.noreply.github.com>
2025-10-07 10:36:17 +0530

    Update fin.py

--- fin.py
+++ fin.py
@@ -1,5 +1,5 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg
+import psycopg.rows
 from datetime import datetime
 
 def run_induction(required_count=3):
@@ -18,12 +18,12 @@ def run_induction(required_count=3):
 
 # ---------------- Database connection ----------------
 def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="KML_dat",
+    return psycopg.connect(
+        host="db.pofumrgccrhearjjlwhv.supabase.co",
+        dbname="postgres",
         user="postgres",
         password="02496",
-        port="5432"
+        port=5432
     )
 
 # ---------------- Weights (tune these as per KMRL priorities) ----------------
@@ -70,132 +70,145 @@ def geometry_component(train):
 
 # ---------------- Main algorithm ----------------
 def generate_induction_list(required_count):
-    conn = get_connection()
-    curr = conn.cursor(cursor_factory=RealDictCursor)
+    conn = None
+    try:
+        conn = get_connection()
+        curr = conn.cursor(row_factory=psycopg.rows.dict_row)
 
-    curr.execute("""
-        SELECT
-            t.train_id,
-            MAX(CASE WHEN fc.status = 'Valid' THEN 1 ELSE 0 END) AS fitness_valid,
-            MAX(CASE WHEN jc.status = 'Open' THEN 1 ELSE 0 END) AS job_card_open,
-            bc.priority_level,
-            ml.cumulative_km,
-            cs.required,
-            cs.status AS cleaning_status,
-            sp.estimated_shunt_moves
-        FROM train t
-        LEFT JOIN fitness_certificate fc ON t.train_id = fc.train_id
-        LEFT JOIN job_card jc ON t.train_id = jc.train_id
-        LEFT JOIN branding_contract bc ON t.train_id = bc.train_id
-        LEFT JOIN (
-                 SELECT DISTINCT ON (train_id)* 
-                 FROM mileage_log
-                 ORDER BY train_id, log_date DESC
-        ) ml ON t.train_id = ml.train_id
-        LEFT JOIN (
-                 SELECT DISTINCT ON (train_id)* 
-                 FROM cleaning_schedule
-                 ORDER BY train_id, deadline DESC
-        ) cs ON t.train_id = cs.train_id
-        LEFT JOIN stabling_position sp ON t.train_id = sp.train_id
-        GROUP BY t.train_id, bc.priority_level, ml.cumulative_km, cs.required, cs.status, sp.estimated_shunt_moves
-    """)
-    
-    trains = curr.fetchall()
-    conn.close()
+        curr.execute("""
+            SELECT
+                t.train_id,
+                MAX(CASE WHEN fc.status = 'Valid' THEN 1 ELSE 0 END) AS fitness_valid,
+                MAX(CASE WHEN jc.status = 'Open' THEN 1 ELSE 0 END) AS job_card_open,
+                bc.priority_level,
+                ml.cumulative_km,
+                cs.required,
+                cs.status AS cleaning_status,
+                sp.estimated_shunt_moves
+            FROM train t
+            LEFT JOIN fitness_certificate fc ON t.train_id = fc.train_id
+            LEFT JOIN job_card jc ON t.train_id = jc.train_id
+            LEFT JOIN branding_contract bc ON t.train_id = bc.train_id
+            LEFT JOIN (
+                     SELECT DISTINCT ON (train_id)* 
+                     FROM mileage_log
+                     ORDER BY train_id, log_date DESC
+            ) ml ON t.train_id = ml.train_id
+            LEFT JOIN (
+                     SELECT DISTINCT ON (train_id)* 
+                     FROM cleaning_schedule
+                     ORDER BY train_id, deadline DESC
+            ) cs ON t.train_id = cs.train_id
+            LEFT JOIN stabling_position sp ON t.train_id = sp.train_id
+            GROUP BY t.train_id, bc.priority_level, ml.cumulative_km, cs.required, cs.status, sp.estimated_shunt_moves
+        """)
+        
+        trains = curr.fetchall()
 
-    candidates, ibl = [], []
+        candidates, ibl = [], []
 
-    # average mileage
-    mileage_values = [float(t["cumulative_km"]) for t in trains if t["cumulative_km"] is not None]
-    avg_mileage = sum(mileage_values) / len(mileage_values) if mileage_values else 0.0
+        # average mileage
+        mileage_values = [float(t["cumulative_km"]) for t in trains if t["cumulative_km"] is not None]
+        avg_mileage = sum(mileage_values) / len(mileage_values) if mileage_values else 0.0
 
-    for t in trains:
-        if not t["fitness_valid"] or t["job_card_open"]:
-            ibl.append(t)
-        else:
-            fitness = fitness_component(t)
-            branding = branding_component(t)
-            mileage  = mileage_component(t, avg_mileage)
-            cleaning = cleaning_component(t)
-            geometry = geometry_component(t)
-            score = (
-                WEIGHTS["fitness"]  * fitness +
-                WEIGHTS["branding"] * branding +
-                WEIGHTS["mileage"]  * mileage +
-                WEIGHTS["cleaning"] * cleaning +
-                WEIGHTS["geometry"] * geometry
-            )
-            t["score"] = score
-            candidates.append(t)
+        for t in trains:
+            if not t["fitness_valid"] or t["job_card_open"]:
+                ibl.append(t)
+            else:
+                fitness = fitness_component(t)
+                branding = branding_component(t)
+                mileage  = mileage_component(t, avg_mileage)
+                cleaning = cleaning_component(t)
+                geometry = geometry_component(t)
+                score = (
+                    WEIGHTS["fitness"]  * fitness +
+                    WEIGHTS["branding"] * branding +
+                    WEIGHTS["mileage"]  * mileage +
+                    WEIGHTS["cleaning"] * cleaning +
+                    WEIGHTS["geometry"] * geometry
+                )
+                t["score"] = score
+                candidates.append(t)
 
-    ranked = sorted(candidates, key=lambda x: x["score"], reverse=True)
-    induction = ranked[:required_count]
-    standby = ranked[required_count:]
+        ranked = sorted(candidates, key=lambda x: x["score"], reverse=True)
+        induction = ranked[:required_count]
+        standby = ranked[required_count:]
 
-    return induction, standby, ibl
+        return induction, standby, ibl
+    finally:
+        if conn:
+            curr.close()
+            conn.close()
 
 # ---------------- Database storage ----------------
 def create_induction_table():
-    conn = get_connection()
-    curr = conn.cursor()
-    curr.execute("""
-        CREATE TABLE IF NOT EXISTS train_induction_list (
-            id SERIAL PRIMARY KEY,
-            train_id INT NOT NULL,
-            list_type VARCHAR(20) NOT NULL,
-            score NUMERIC,
-            fitness_valid BOOLEAN,
-            job_card_open BOOLEAN,
-            branding_level VARCHAR(20),
-            cumulative_km NUMERIC,
-            cleaning_required BOOLEAN,
-            cleaning_status VARCHAR(20),
-            estimated_shunt_moves NUMERIC,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    """)
-    conn.commit()
-    curr.close()
-    conn.close()
+    conn = None
+    try:
+        conn = get_connection()
+        curr = conn.cursor()
+        curr.execute("""
+            CREATE TABLE IF NOT EXISTS train_induction_list (
+                id SERIAL PRIMARY KEY,
+                train_id INT NOT NULL,
+                list_type VARCHAR(20) NOT NULL,
+                score NUMERIC,
+                fitness_valid BOOLEAN,
+                job_card_open BOOLEAN,
+                branding_level VARCHAR(20),
+                cumulative_km NUMERIC,
+                cleaning_required BOOLEAN,
+                cleaning_status VARCHAR(20),
+                estimated_shunt_moves NUMERIC,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        """)
+        conn.commit()
+    finally:
+        if conn:
+            curr.close()
+            conn.close()
 
 def save_lists_to_db(induction, standby, ibl):
-    conn = get_connection()
-    curr = conn.cursor()
-    # clear old data
-    curr.execute("DELETE FROM train_induction_list")
-    conn.commit()
+    conn = None
+    try:
+        conn = get_connection()
+        curr = conn.cursor()
+        # clear old data
+        curr.execute("DELETE FROM train_induction_list")
+        conn.commit()
 
-    def insert_train(t, list_type):
-        curr.execute("""
-            INSERT INTO train_induction_list (
-                train_id, list_type, score, fitness_valid, job_card_open,
-                branding_level, cumulative_km, cleaning_required, cleaning_status,
-                estimated_shunt_moves
-            ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-        """, (
-            t["train_id"],
-            list_type,
-            t.get("score"),
-            bool(t.get("fitness_valid")),   # CAST integer to boolean
-            bool(t.get("job_card_open")),   # CAST integer to boolean
-            t.get("priority_level"),
-            t.get("cumulative_km"),
-            bool(t.get("required")),        # CAST integer/None to boolean
-            t.get("cleaning_status"),
-            t.get("estimated_shunt_moves")
-        ))
+        def insert_train(t, list_type):
+            curr.execute("""
+                INSERT INTO train_induction_list (
+                    train_id, list_type, score, fitness_valid, job_card_open,
+                    branding_level, cumulative_km, cleaning_required, cleaning_status,
+                    estimated_shunt_moves
+                ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            """, (
+                t["train_id"],
+                list_type,
+                t.get("score"),
+                bool(t.get("fitness_valid")),   # CAST integer to boolean
+                bool(t.get("job_card_open")),   # CAST integer to boolean
+                t.get("priority_level"),
+                t.get("cumulative_km"),
+                bool(t.get("required")),        # CAST integer/None to boolean
+                t.get("cleaning_status"),
+                t.get("estimated_shunt_moves")
+            ))
 
-    for t in induction:
-        insert_train(t, "Induction")
-    for t in standby:
-        insert_train(t, "Standby")
-    for t in ibl:
-        insert_train(t, "IBL")
+        for t in induction:
+            insert_train(t, "Induction")
+        for t in standby:
+            insert_train(t, "Standby")
+        for t in ibl:
+            insert_train(t, "IBL")
 
-    conn.commit()
-    curr.close()
-    conn.close()
+        conn.commit()
+    finally:
+        if conn:
+            curr.close()
+            conn.close()
 
 # ---------------- Main block ----------------
 if __name__ == "__main__":
